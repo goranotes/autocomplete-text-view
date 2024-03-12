9f38c158fcb156244bc13ef37a8c6a44a278902d
gora <anggara.dwikuntoro@gmail.com>
2024-03-12 12:34:22 +0700

    [Remove] Optimize imports

--- app/src/main/java/com/goranotes/autocompletetextview/adapter/AutoCompleteAdapter.kt
+++ app/src/main/java/com/goranotes/autocompletetextview/adapter/AutoCompleteAdapter.kt
@@ -5,11 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Filter
-import androidx.annotation.NonNull
 import com.goranotes.autocompletetextview.databinding.ItemDataAutocompleteBinding
 import com.goranotes.autocompletetextview.model.DataItemCustomerResponse
-import java.util.Locale
 
 class AutoCompleteAdapter(
     context: Context,
